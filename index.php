de7ee92a4b9ee0111c9494500bc4206a6346b772
giulymezz <ggiulia.mmazzi@gmail.com>
2025-12-01 17:38:13 +0100

    Refactor: separate form into form.php to improve layout organization

--- index.php
+++ index.php
@@ -17,44 +17,7 @@ $pageTitle = "Users";
 
 ob_start();
 
-?>
-
-<h1 class="page-title">USERS</h1>
-
-<div class="form-wrapper">
-    <form method="post" class="filter-form">
-
-        <label>Active:</label>
-        <select name="active">
-            <option value="">All</option>
-            <option value="1">Active</option>
-            <option value="0">Inactive</option>
-        </select>
-
-        <label>From:</label>
-        <input type="text" name="from" placeholder="d/m/Y H:i:s">
-
-        <label>To:</label>
-        <input type="text" name="to" placeholder="d/m/Y H:i:s">
-
-        <label>Name starts with:</label>
-        <input type="text" name="name">
-
-        <label>Surname starts with:</label>
-        <input type="text" name="surname">
-
-        <label>View:</label>
-        <select name="view">
-            <option value="table">Table</option>
-            <option value="thumb">Thumb</option>
-        </select>
-
-        <button type="submit" class="btn">Search</button>
-
-    </form>
-</div>
-
-<?php
+require __DIR__ . '/views/form.php';
 
 $content = ob_get_clean();
 
---
+++ views/form.php
@@ -0,0 +1,34 @@
+<h1 class="page-title">USERS</h1>
+
+<div class="form-wrapper">
+<form method="post" class="filter-form">
+
+    <label>Active:</label>
+    <select name="active">
+        <option value="">All</option>
+        <option value="1">Active</option>
+        <option value="0">Inactive</option>
+    </select>
+
+    <label>From:</label>
+    <input type="text" name="from" placeholder="d/m/Y H:i:s">
+
+    <label>To:</label>
+    <input type="text" name="to" placeholder="d/m/Y H:i:s">
+
+    <label>Name starts with:</label>
+    <input type="text" name="name">
+
+    <label>Surname starts with:</label>
+    <input type="text" name="surname">
+
+    <label>View:</label>
+    <select name="view">
+        <option value="table">Table</option>
+        <option value="thumb">Thumb</option>
+    </select>
+
+    <button type="submit" class="btn">Search</button>
+
+</form>
+</div>
